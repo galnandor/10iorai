89cc4a3e81f4b900d16a79bba9cbebe23f0cb649
Gal Nandor <nandoraron6@gmail.com>
2023-02-27 14:21:49 +0100

    második

--- függvények.py
+++ függvények.py
@@ -17,3 +17,40 @@ print(dup(szam))
 
 print(koszont("Pipacs"))
 print(dup("alma"))
+
+def paros(szam):
+    if type(szam) == int:
+        if szam % 2 == 0:
+            return True
+        else:
+            return False
+
+paros(paros(13))
+paros(paros(12))
+print(paros("aa"))
+print(paros(12.45))
+
+y=33
+def localGlobal(szam):
+    x=szam  # local változó
+    y=szam  # globális
+x=12
+localGlobal(22)
+print(x)
+print(y)
+
+def rekurziv(x):
+    x+=1
+    print(x)
+    if x <= 10:
+        rekurziv(x)
+
+rekurziv(1)
+
+def fibbon(n1,n2,db):
+    print(n1,n2)
+    db+=1
+    if db < 10:
+        fibbon(n2,n1+n2,db)
+
+fibbon(1,1,1)

239d09021fd3745ab3130ad6ff31f007109c7dd5
Gal Nandor <nandoraron6@gmail.com>
2023-02-27 13:41:23 +0100

    első

---
+++ függvények.py
@@ -0,0 +1,19 @@
+def koszont( nev):
+    print("Szia "+ nev+"!")
+
+koszont("Pista")
+koszont("Zoli")
+
+def dup(szam):
+    szam = szam *2
+    #print(szam)
+    return szam
+
+szam=12
+print(szam)
+dup(szam)
+print(szam)
+print(dup(szam))
+
+print(koszont("Pipacs"))
+print(dup("alma"))
